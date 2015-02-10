e68cfd5e867a200af3a0a2d07703a2f51960cb0f
fereydoun <gholami.f84@gmail.com>
2015-02-10 09:39:30 +0330

    xmlbuilder 9:39 1393/11/20

--- src/main/java/entities/JSONReader.java
+++ src/main/java/entities/JSONReader.java
@@ -1,14 +1,10 @@
 package entities;
-
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 
 public class JSONReader {
 
--- src/main/java/entities/XMLBuilder.java
+++ src/main/java/entities/XMLBuilder.java
@@ -1,4 +1,87 @@
 package entities;
 
+import exceptions.XMLException;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
 public class XMLBuilder {
+
+    private Document document;
+    private String fileName;
+
+    public XMLBuilder(String fileName) throws Exception
+    {
+        if (fileName == "")
+            throw new XMLException("File name is empty");
+        this.fileName = fileName;
+        createDocument(fileName);
+    }
+
+    public void createDocument(String fileName) throws Exception
+    {
+        try {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            if ((new File(fileName)).exists())
+                document = documentBuilder.parse(fileName);
+            else
+                document=documentBuilder.newDocument();
+        }catch(Exception ex){
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    public Element addTag(String tagName){
+
+        Element tag=this.document.createElement(tagName);
+        this.document.appendChild(tag);
+        return tag;
+    }
+    public Element addTag(String tagName,String tagValue)
+    {
+        Element tag=this.document.createElement(tagName);
+        tag.appendChild(this.document.createTextNode(tagValue));
+        this.document.appendChild(tag);
+        return tag;
+    }
+
+    public Element addTag(Element tag,String childName,String childValue){
+
+        Element childTag=this.document.createElement(childName);
+        childTag.appendChild(this.document.createTextNode(childValue));
+        tag.appendChild(childTag);
+        return  childTag;
+    }
+
+    public Attr createAttribute(Element tag,String attrName,String attrValue){
+
+        Attr attr=this.document.createAttribute(attrName);
+        attr.setValue(attrValue);
+        tag.setAttributeNode(attr);
+        return attr;
+    }
+
+    public void writeToXMLFile() throws Exception{
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource=new DOMSource(this.document);
+            StreamResult streamResult=new StreamResult();
+            transformer.transform(domSource,streamResult);
+
+        } catch (Exception ex) {
+            throw new XMLException("Error in write to file");
+        }
+    }
+
+
 }
